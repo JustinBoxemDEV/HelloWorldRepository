280dddde06719eaf430dcc741d84f6c03aa31ff0
DavidHoeksema <112412@student.drenthecollege.nl>
2017-03-24 09:53:50 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/helloworld/HelloWorld.java

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,8 +16,8 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //tekst david <-----
+        //tekst -
     }
     
 }
-//nfjdnsfskjnfjknsdkjfs test david DSZFXGCHVJBKNLM',TRXYTCUVIHOJ
+//
